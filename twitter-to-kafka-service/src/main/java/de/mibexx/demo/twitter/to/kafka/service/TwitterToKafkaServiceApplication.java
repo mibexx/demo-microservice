9b3fc250d0e9cc80b286fb911cef9745d496a033
Mike Bertram <contact@mibexx.de>
2021-10-19 16:03:28 +0200

    Read data from twitter

--- twitter-to-kafka-service/src/main/java/de/mibexx/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/de/mibexx/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,6 +1,7 @@
 package de.mibexx.demo.twitter.to.kafka.service;
 
 import de.mibexx.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import de.mibexx.demo.twitter.to.kafka.service.runner.impl.TwitterKafkaStreamRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -14,9 +15,14 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
     private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
 
     private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+    private final TwitterKafkaStreamRunner twitterKafkaStreamRunner;
 
-    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData) {
+    public TwitterToKafkaServiceApplication(
+            TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData,
+            TwitterKafkaStreamRunner twitterKafkaStreamRunner
+    ) {
         this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+        this.twitterKafkaStreamRunner = twitterKafkaStreamRunner;
     }
 
     public static void main(String[] args) {
@@ -28,5 +34,7 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
         LOG.info("App starts...");
         LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
         LOG.info(twitterToKafkaServiceConfigData.getWelcomeMessage());
+
+        twitterKafkaStreamRunner.start();
     }
 }
