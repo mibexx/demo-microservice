646e4ddbc92dc4941bba27760a5f3ba4dded0cdc
Mike Bertram <contact@mibexx.de>
2021-11-02 08:51:42 +0100

    Added config server basic auth

--- twitter-to-kafka-service/src/main/java/de/mibexx/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/de/mibexx/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,6 +1,5 @@
 package de.mibexx.demo.twitter.to.kafka.service;
 
-import de.mibexx.demo.config.TwitterToKafkaServiceConfigData;
 import de.mibexx.demo.twitter.to.kafka.service.init.StreamInitializer;
 import de.mibexx.demo.twitter.to.kafka.service.runner.StreamRunner;
 import org.slf4j.Logger;
@@ -9,8 +8,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "de.mibexx.demo")
